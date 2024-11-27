6174ed4468557d38071ae29d1d03f0dddd69c0bc
Yiming <yimili@student.ethz.ch>
2024-11-27 19:21:53 +0100

    initial commit

---
+++ fretboard.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+
+def draw_fretboard(show_notes=True, chord=[[0,0]]):
+    chromatic_scale = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'bB', 'B']
+    
+    fig, ax = plt.subplots(figsize=(10, 1.5), facecolor='black')  # Adjust size to fit your layout
+    num_frets = 12  # Number of frets on the guitar fretboard
+    num_strings = 6
+
+    # Draw the strings y-coordinates
+    for string in range(num_strings):
+        ax.plot([0, num_frets], [string, string], color=(0.8, 0.643, 0.0), lw=4)  # Line width as string thickness
+
+    # Draw the frets x-coordinates
+    ax.plot([0, 0], [-0.5, 5.5], color='white', lw=2)  # fret 0 is white 
+    for fret in range(1,num_frets + 1):
+        ax.plot([fret, fret], [-0.5, 5.5], color=(0.6, 0.482, 0.0), lw=2)  # Line width as fret thickness
+
+    # Optionally add notes
+    if show_notes:
+        notes = ['E', 'A', 'D', 'G', 'B', 'E']  # Open string notes for standard tuning
+        for string in range(6):
+            start_note = notes[string]
+            start_index = chromatic_scale.index(start_note)
+
+            for fret in range(num_frets + 1):
+                note_index = (start_index + fret) % 12 
+                note = chromatic_scale[note_index]    
+                ax.text(fret-0.2, string, note, color='white', ha='center', va='center', fontweight='bold', alpha=0.75)
+    
+    highlighted_notes = chord
+    for highlighted_note in highlighted_notes:
+        # Add the rectangle patch to highlight the segment
+        highlight_fret = highlighted_note[0]
+        highlight_string = highlighted_note[1]
+        highlight_width = 0.38          #golden ratio
+        highlight_height = 0.3
+        ax.add_patch(Rectangle((highlight_fret-highlight_width, highlight_string - highlight_height/2), highlight_width-0.025, highlight_height, color=(0.627, 0.078, 0.157), alpha=1.0, zorder=3))
+    
+
+#wine red color=(0.627, 0.078, 0.157)
+    ax.set_ylim(-0.5, 5.5)
+    ax.axis('off')  # Hide axes
+    return fig
+
+chord1=[[0,0],[1,1],[3,4]]
+a_minor = [[0,1],[2,2],[2,3],[1,4],[0,5]]
+draw_fretboard(show_notes=True,chord = a_minor)
+plt.show()
---
+++ prototype1.py
@@ -0,0 +1,100 @@
+from PIL import Image, ImageDraw
+import streamlit as st
+from fretboard import draw_fretboard
+from io import BytesIO
+
+st.set_page_config(layout="wide")
+
+# Set up session state for showing the ellipse
+if 'show_notes' not in st.session_state:
+    st.session_state.show_notes = True  # Initialize state to show the ellipse
+
+# Using containers for vertical organization
+intro_container = st.container()
+with intro_container:
+    st.title("Guitar Fretboard Design App")
+    st.write("This section provides an overview of the application and user instructions.")
+
+# Layout with left sidebar, main content, and right sidebar
+left_sidebar, spacer, image_col1, right_sidebar = st.columns([1,1, 4, 1])  # Adjust the ratio as needed
+
+with left_sidebar:
+    st.title("Choose Scale")
+    scale_ionian = st.button("Ionian")
+    scale_dorian = st.button("Dorian")
+    scale_phrygian = st.button("Phrygian")
+    scale_lydian = st.button("Lydian")
+    scale_myxologidan = st.button("Myxolodian")
+    scale_aoelian = st.button("Aoelian")
+    scale_locrian = st.button("Locrian")
+
+    if scale_ionian:
+        st.write("Iolian Mode")
+    elif scale_dorian:
+        st.write("Dorian Mode")
+    elif scale_phrygian:
+        st.write("Phrygian Mode")
+    elif scale_lydian:
+        st.write("Lydian Mode")
+    elif scale_myxologidan:
+        st.write("Myxolodian Mode")
+    elif scale_aoelian:
+        st.write("Aoelian Mode")
+    elif scale_locrian:
+        st.write("Locrian Mode")
+
+with spacer:
+    st.title("Choose Key")
+
+with right_sidebar:
+    st.title("Right Sidebar")
+    button4 = st.button("Show Notes")
+    button5 = st.button("Hide Notes")
+    button6 = st.button("Button 6")
+    if button4:
+        st.write("Button 4 was pressed")
+        st.session_state.show_notes = True
+    elif button5:
+        st.write("Button 5 was pressed")
+        st.session_state.show_notes = False
+
+# Using an expander for additional options
+options_expander = st.expander("Advanced Options")
+with options_expander:
+    st.checkbox("Show Grid Lines")
+    st.checkbox("Enable Editing Mode")
+
+# Footer or additional info section
+footer_container = st.container()
+with footer_container:
+    st.write("Created by Your Name - 2024")
+    st.write("Additional information, links, or tutorials can be placed here.")
+
+
+##################FRETBOARDS#################
+a_minor = [[0,1],[2,2],[2,3],[1,4],[0,5]]
+fretboard_figures = [
+    draw_fretboard(show_notes=st.session_state.show_notes, chord = a_minor),
+    draw_fretboard(show_notes=st.session_state.show_notes, chord = a_minor),
+    draw_fretboard(show_notes=st.session_state.show_notes, chord = a_minor),
+]
+
+def save_plot_as_file(fig, file_format="png"):
+    buffer = BytesIO()
+    fig.savefig(buffer, format=file_format)
+    buffer.seek(0)
+    return buffer
+
+
+with image_col1:
+    st.title("1st Fretboard")
+    for i, fig in enumerate(fretboard_figures):
+        st.pyplot(fig)
+        st.download_button(
+            label=f"Download Fretboard1 {i + 1} as PDF",
+            data=save_plot_as_file(fig, file_format="pdf"),
+            file_name=f"fretboard_{i + 1}.pdf",
+            mime="application/pdf"
+        )
+
+
